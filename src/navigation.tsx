544d1d988a581cba0ceb31696d09454109610c07
Wendy Nam <142412339+Wendy-Nam@users.noreply.github.com>
2024-10-21 18:35:35 +0900

    이슈 해결 안됨 / 타입 관련 추가내용 삭제

--- src/navigation.tsx
+++ src/navigation.tsx
@@ -29,34 +29,3 @@ const AppNavigator = () => {
 };
 
 export default AppNavigator;
-
-// import React from 'react';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createStackNavigator } from '@react-navigation/stack';
-// import { RootStackParamList } from '@types';
-
-// import Example from '@screens/Example';
-// import Example2 from '@screens/Example2';
-// import TestingPage from '@screens/TestingPage';
-
-// const Stack = createStackNavigator<RootStackParamList>();
-
-// const AppNavigator = () => {
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator
-//         initialRouteName="TestingPage"
-//         screenOptions={{
-//           headerShown: false,
-//         }}
-//       >
-//         {/* example */}
-//         <Stack.Screen name="Example" component={Example} />
-//         <Stack.Screen name="Example2" component={Example2} />
-//         <Stack.Screen name="TestingPage" component={TestingPage} />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// };
-
-// export default AppNavigator;
