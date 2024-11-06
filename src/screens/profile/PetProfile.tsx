c0e01cb5a7ec7cebf72bef611f678f792f741759
Kiki1875b <kevinheo0413@gmail.com>
2024-11-06 16:00:48 +0900

    PetProfile 컴포넌트 리팩토링

--- src/screens/profile/PetProfile.tsx
+++ src/screens/profile/PetProfile.tsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import { View, Text, Image, StyleSheet, TouchableOpacity, ScrollView, SafeAreaView, Alert } from 'react-native';
 import { useNavigation, RouteProp } from '@react-navigation/native';
-import registerPhoto from '@image/frame/registerPhoto.png';
 import { deletePet } from '@src/api/petApi';
-import { PetDetails } from '@src/utils/constants/types';
+import StylizedText from '@src/components/common/StylizedText';
+import Avatar from '@src/components/common/Avatar';
+import AvatarPlaceholder from '@image/placeholder/dog.jpg';
+import { RoundedTextButton } from '@src/components/common/RoundedButton';
 
-// 초기 데이터(임시)
+// TODO: 일일 산책 박스 필요
+// TODO: 반려동물 상세정보에서 text box 필요
+
 const mockData = {
   steps: {
     current: 1900,
@@ -86,269 +90,150 @@ const PetProfile: React.FC<{ route: RouteProp<RootStackParamList, 'PetProfile'> 
 
 
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <ScrollView contentContainerStyle={styles.scrollViewContent}>
-          <TouchableOpacity onPress={handleHealthInfo} style={styles.healthInfoButton}>
-                <Text style={styles.healthInfoButtonText}>보건정보 조회하기</Text>
+    <SafeAreaView className="flex-1 bg-white">
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        {/* 상단 버튼들 */}
+        
+
+        <View className="flex-1 px-5 bg-white items-center">
+          {/* 뒤로가기 버튼 */}
+          <TouchableOpacity onPress={handleBackButton} className="self-start mt-5">
+            <Text className="text-gray-500 text-2xl">{'<'}</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity onPress={handlePetDelete} style={styles.healthInfoButton}>
-              <Text style={styles.healthInfoButtonText}>반려동물 삭제하기</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity onPress={handlePetUpdate} style={styles.healthInfoButton}>
-              <Text style={styles.healthInfoButtonText}>반려동물 수정하기</Text>
-          </TouchableOpacity>
-        <View style={styles.container}>
-          <TouchableOpacity onPress={handleBackButton} style={styles.backButton}>
-            <Text style={styles.backButtonText}>{'<'}</Text>
-          </TouchableOpacity>
-          <View style={styles.profileSection}>
-            <Image
-              source={imageUrl ? { uri: imageUrl } : registerPhoto}
-              style={styles.profileImage}
+          {/* 프로필 섹션 */}
+          <View className="items-center mt-5 mb-5 w-full">
+            <Avatar
+              source={imageUrl || AvatarPlaceholder}
+              size={150}
             />
-            <Text style={styles.name}>{name}</Text>
+            <View className='pt-4'>
+              <StylizedText type='header1'>{name}</StylizedText>
+            </View>
 
-            <View style={styles.calorieInfoContainer}>
-              <View style={styles.calorieInfo}>
-                <Text style={styles.breed}>예상 칼로리 소모량</Text>
-                <Text style={styles.calories}>{mockData.estimatedCalories} kcal</Text>
-              </View>
-            </View>
-          </View>
-          
-          <View style={styles.card}>
-            <Text style={styles.statsTitle}>일일 산책</Text>
-            <View style={styles.statItem}>
-              <View style={styles.statLabels}>
-                <Text style={styles.labelText}>걸음 수</Text>
-                <Text style={styles.valueText}>{mockData.steps.current} / {mockData.steps.goal}</Text>
-              </View>
-              <View style={styles.statBarOuter}>
-                <View style={[styles.statBarInner, { width: `${stepsPercentage}%` }]} />
-              </View>
-            </View>
+
             
-            <View style={styles.statItem}>
-              <View style={styles.statLabels}>
-                <Text style={styles.labelText}>시간</Text>
-                <Text style={styles.valueText}>{mockData.minutes.current} / {mockData.minutes.goal} 분</Text>
+          </View>
+          <View className="w-full items-start mt-2">
+            <View className="mt-1 pl-5 pt-5">
+              <StylizedText type="header2">예상 칼로리 소모량</StylizedText>
+              <StylizedText type="body1">{mockData.estimatedCalories} kcal</StylizedText>
+            </View>
+          </View>
+
+          {/* 일일 산책 카드 */}
+          <View className="w-full bg-silver rounded-xl p-5 mt-5 shadow">
+            <View className='pb-5'>
+              <StylizedText type='header1'>일일 산책</StylizedText >
+            </View>
+
+            {/* 걸음 수 */}
+            <View className="mb-5">
+              <View className="flex-row justify-between mb-2">
+                <StylizedText type='header2'>걸음 수 </StylizedText>
+                <StylizedText type='header2'>
+                  {mockData.steps.current} / {mockData.steps.goal}
+                </StylizedText>
               </View>
-              <View style={styles.statBarOuter}>
-                <View style={[styles.statBarInner, { width: `${minutesPercentage}%` }]} />
+              <View className="h-7 bg-skyblue rounded-full overflow-hidden p-1">
+                <View
+                  className="h-full bg-blue rounded-full"
+                  style={{ width: `${stepsPercentage}%` }}
+                />
+              </View>
+            </View>
+
+            {/* 시간 */}
+            <View className="mb-5">
+              <View className="flex-row justify-between mb-2">
+                <StylizedText type='header2'>시간</StylizedText>
+                <StylizedText type='header2'>
+                  {mockData.minutes.current} / {mockData.minutes.goal} 분
+                </StylizedText>
+              </View>
+              <View className="h-7 bg-skyblue rounded-full overflow-hidden p-1">
+                <View
+                  className="h-full bg-blue rounded-full"
+                  style={{ width: `${minutesPercentage}%` }}
+                />
               </View>
             </View>
           </View>
-          
+
+          {/* 상세 정보 */}
+    
           {showDetail && (
             <>
-              <View style={styles.detailCard}>
-                <Text style={styles.detailLabel}>{getLabel('petType')}</Text>
-                <Text style={styles.detailValue}>{petType}</Text>
+              <View className="w-full bg-white rounded-xl p-5 mt-5 border border-gray-300 flex-row justify-between items-center">
+               <StylizedText type='header2'>{getLabel('petType')}</StylizedText>
+               <StylizedText type='header2'>{petType}</StylizedText>
               </View>
-              <View style={styles.detailCard}>
-                <Text style={styles.detailLabel}>{getLabel('gender')}</Text>
-                <Text style={styles.detailValue}>{gender}</Text>
+
+              <View className="w-full bg-white rounded-xl p-5 mt-5 border border-gray-300 flex-row justify-between items-center">
+                <StylizedText type='header2'>{getLabel('gender')}</StylizedText>
+                <StylizedText type='header2'>{gender}</StylizedText>
               </View>
-              <View style={styles.detailCard}>
-                <Text style={styles.detailLabel}>{getLabel('weight')}</Text>
-                <Text style={styles.detailValue}>{weight} kg</Text>
+
+              <View className="w-full bg-white rounded-xl p-5 mt-5 border border-gray-300 flex-row justify-between items-center">
+                <StylizedText type='header2'>{getLabel('weight')}</StylizedText>
+                <StylizedText type='header2'>{weight} kg</StylizedText>
               </View>
-              <View style={styles.detailCard}>
-                <Text style={styles.detailLabel}>{getLabel('age')}</Text>
-                <Text style={styles.detailValue}>{age} 세</Text>
+
+              <View className="w-full bg-white rounded-xl p-5 mt-5 border border-gray-300 flex-row justify-between items-center">
+                <StylizedText type='header2'>{getLabel('age')}</StylizedText>
+                <StylizedText type='header2'>{age} 세</StylizedText>
               </View>
+
+              <View className="justify-around p-4">
+                <View className='pt-5'>
+                  <RoundedTextButton
+                    content={"보건정보 조회하기"}
+                    textType='header2'
+                    widthOption="full"
+                    color="bg-primary"
+                    onPress={handleHealthInfo}
+                  />
+                </View>
+
+                <View className='pt-5'>
+                  <RoundedTextButton
+                    content={"반려동물 삭제하기"}
+                    textType='header2'
+                    widthOption="full"
+                    color="bg-primary"
+                    onPress={handlePetDelete}
+                  />
+                </View>
+
+                <View className='pt-5'>
+                  <RoundedTextButton
+                    content={"반려동물 수정하기"}
+                    textType='header2'
+                    widthOption="full"
+                    color="bg-primary"
+                    onPress={handlePetUpdate}
+                  />
+              </View>
+            </View>
             </>
           )}
-          
-          <TouchableOpacity style={styles.button} onPress={toggleDetail}>
-            <Text style={styles.buttonText}>
-              {showDetail ? "정보 숨기기" : "정보 펼쳐보기"}
-            </Text>
-          </TouchableOpacity>
+
+          {/* 정보 토글 버튼 */}
+          <View className='pt-5'>
+            <RoundedTextButton
+              content={showDetail ? '정보 숨기기' : '정보 펼쳐보기'}
+              textType='header2'
+              widthOption="full"
+              color="bg-primary"
+              onPress={toggleDetail}
+            />
+          </View>
+
         </View>
       </ScrollView>
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  scrollViewContent: {
-    flexGrow: 1,
-  },
-  container: {
-    flex: 1,
-    paddingHorizontal: 20,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-  },
-  backButton: {
-    alignSelf: 'flex-start',
-    marginTop: 20,
-    marginLeft: 0,
-  },
-  backButtonText: {
-    fontSize: 24,
-    color: '#888',
-  },
-  profileSection: {
-    alignItems: 'center',
-    marginTop: 20,
-    marginBottom: 20,
-    width: '100%', 
-  },
-  profileImage: {
-    width: 150,
-    height: 150,
-    borderRadius: 100,
-  },
-  name: {
-    color: 'black',
-    fontSize: 34,
-    paddingTop: 10,
-    fontWeight: 'bold',
-    marginTop: 10,
-  },
-  calorieInfo: {
-    alignItems: 'flex-start',
-    alignSelf: 'flex-start',
-    marginTop: 5,
-    color: 'black',
-    fontWeight: 'bold',
-    paddingLeft: 10,
-    paddingTop: 20,
-  },
-  breed: {
-    fontWeight: 'bold',
-    color: 'black',
-    fontSize: 20,
-    textAlign: 'left', 
-  },
-  calories: {
-    color: '#666',
-    fontSize: 18,
-    textAlign: 'left', 
-  },
-  card: {
-    width: '100%',
-    backgroundColor: 'white',
-    borderRadius: 15,
-    padding: 20,
-    marginTop: 20,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.1,
-    shadowRadius: 3.84,
-    elevation: 5,
-  },
-  statsTitle: {
-    color: 'black',
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 15,
-  },
-  statItem: {
-    marginBottom: 20,
-  },
-  statLabels: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 8,
-  },
-  labelText: {
-    color: '#666',
-    fontSize: 16,
-    fontWeight: '600',
-  },
-  valueText: {
-    color: '#333',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  statBarOuter: {
-    height: 30,
-    backgroundColor: '#E7F9ED',
-    borderRadius: 20,
-    overflow: 'hidden',
-    padding: 3,
-  },
-  statBarInner: {
-    height: '100%',
-    backgroundColor: '#85E0A3',
-    borderRadius: 15,
-  },
-  detailItem: {
-    marginBottom: 15,
-  },
-  detailCard: {
-    width: '100%',
-    backgroundColor: 'white',
-    borderRadius: 15,
-    padding: 20,
-    marginTop: 20,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  detailLabel: {
-    color: '#666',
-    fontSize: 16,
-    fontWeight: '600',
-  },
-  detailValue: {
-    color: '#333',
-    fontSize: 16,
-    fontWeight: 'bold',
-    textAlign: 'right',
-  },
-  button: {
-    width: '100%',
-    backgroundColor: '#73A8BA',
-    padding: 15,
-    borderRadius: 25,
-    alignItems: 'center',
-    marginTop: 20,
-    marginBottom: 20,
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  sectionTitle: {
-    color: 'black',
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginTop: 20,
-    marginBottom: 10,
-    alignSelf: 'flex-start',
-  },
-  calorieInfoContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginTop: 8,
-  },
-  healthInfoButton: {
-    backgroundColor: '#4CAF50',
-    paddingVertical: 8,
-    paddingHorizontal: 12,
-    borderRadius: 8,
-  },
-  healthInfoButtonText: {
-    color: '#fff',
-    fontSize: 14,
-    fontWeight: 'bold',
-  },
-});
 
 export default PetProfile;
