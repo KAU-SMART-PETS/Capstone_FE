095f651acdd7cc6136114afa1116e4df44624344
Wendy Nam <142412339+Wendy-Nam@users.noreply.github.com>
2024-11-06 13:15:17 +0900

    Fix: 빌드와 실행이슈 해결 (패키지 의존성)
    This reverts commit a4c36689adbf138cc679881588b450af7c542c70.
    
    실행이슈 해결
    
    의존성 실행이슈 해결
    
    이제 안드로이드에서 실행되는 듯함
    이제 테스트 후 main에 추가수정사항 반영할 예정. 기기스토리지 패키지의존성도 추후해결 예정

--- babel.config.js
+++ babel.config.js
@@ -3,9 +3,6 @@ module.exports = function (api) {
   return {
     presets: [
       'module:@react-native/babel-preset',
-      '@babel/preset-env',
-      '@babel/preset-typescript',
-      '@babel/preset-react'
     ],
     plugins: [
       [
