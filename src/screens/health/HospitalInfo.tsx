a1058f56ea6ff6242e08692340700c0b44094f3c
Kiki1875b <kevinheo0413@gmail.com>
2024-11-07 12:07:41 +0900

    병원 리스트, 상세정보 컴포넌트화

--- src/screens/health/HospitalInfo.tsx
+++ src/screens/health/HospitalInfo.tsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity, Image } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { fetchHospitalInfo } from '@src/api/hospitalApi';
-import axios from 'axios';
 import KakaoMap from './kakaoMap';
+import StylizedText from '@src/components/common/StylizedText';
 
 
 const HospitalInfo = () => {
@@ -25,89 +25,57 @@ const HospitalInfo = () => {
     loadHospitalInfo();
   }, [vetId, latitude, longitude]);
 
+  const handleBackButton = () => {
+    navigation.goBack();
+  };
+
   if (!hospital) {
     return (
-      <SafeAreaView style={styles.container}>
-        <Text>Loading...</Text>
+      <SafeAreaView className="flex-1 bg-white">
+        <StylizedText type='body1'>Loading...</StylizedText>
       </SafeAreaView>
     );
   }
 
   return (
-    <SafeAreaView style={styles.container}>
-      <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButton}>
-        <Text style={styles.backButtonText}>{'<'}</Text>
+    <SafeAreaView className="flex-1 bg-white">
+      <TouchableOpacity onPress={handleBackButton} className="p-2">
+        <Text className="text-2xl text-gray-500">{'<'}</Text>
       </TouchableOpacity>
-      <Text style={styles.title}>{hospital.name}</Text>
-      <View style={styles.infoContainer}>
-        <View style={styles.infoRow}>
-          <Image source={require('@image/icon/phoneVector.png')} style={styles.icon} />
-          <Text style={styles.info}>{hospital.telephone}</Text>
-        </View>
-        <View style={styles.infoRow}>
-          <Image source={require('@image/icon/locationVector.png')} style={styles.icon} />
-          <Text style={styles.info}>{hospital.address}</Text>
-        </View>
-        <View style={styles.infoRow}>
-          <Image source={require('@image/icon/locationVector.png')} style={styles.icon} /> 
-          <Text style={styles.info}>{hospital.vetToMemberDistance} m</Text>
+      <View className="p-5">
+        <StylizedText type="header1">{hospital.name}</StylizedText>
+
+        <View className="bg-white border-b-4 border-gray-100 p-5">
+          <View className="flex-row items-center mb-2.5">
+            <Image
+              source={require('@image/icon/phoneVector.png')}
+              className="w-[30px] h-[30px] mr-2.5"
+            />
+            <StylizedText type="header2">{hospital.telephone}</StylizedText>
+          </View>
+          <View className="flex-row items-center mb-2.5">
+            <Image
+              source={require('@image/icon/locationVector.png')}
+              className="w-[30px] h-[30px] mr-2.5"
+            />
+            <StylizedText type="header2">{hospital.address}</StylizedText>
+          </View>
+          <View className="flex-row items-center mb-2.5">
+            <Image
+              source={require('@image/icon/locationVector.png')}
+              className="w-[30px] h-[30px] mr-2.5"
+            />
+            <StylizedText type="header2">
+              {Math.floor(hospital.vetToMemberDistance)} m
+            </StylizedText>
+          </View>
         </View>
       </View>
-      <View style={styles.mapContainer}>
+      <View className="flex-1 m-5 rounded-lg overflow-hidden border border-gray-300">
         <KakaoMap latitude={hospital.latitude} longitude={hospital.longitude} />
       </View>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ffffff', 
-  },
-  backButton: {
-    padding: 10,
-  },
-  backButtonText: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#555555', 
-  },
-  title: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    paddingLeft: 20,
-    paddingTop: 20,
-    color: '#333333',
-  },
-  infoContainer: {
-    padding: 20,
-    backgroundColor: 'white',
-    borderBottomWidth: 5,
-    borderBottomColor: '#F9F9F9',
-  },
-  infoRow: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 10,
-  },
-  icon: {
-    width: 30,
-    height: 30,
-    marginRight: 10,
-  },
-  info: {
-    color: 'black',
-    fontSize: 20,
-  },
-  mapContainer: {
-    flex: 1,
-    margin: 20,
-    borderRadius: 10,
-    overflow: 'hidden',
-    borderWidth: 1,
-    borderColor: '#e0e0e0',
-  },
-});
 
 export default HospitalInfo;
