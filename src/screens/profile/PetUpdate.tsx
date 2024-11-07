694c4c8e2612e7995f6d7efcdbf65f0ed8081e61
Kiki1875b <kevinheo0413@gmail.com>
2024-11-07 10:49:59 +0900

    feat: pet update pet register과 동일하도록 리팩토링

--- src/screens/profile/PetUpdate.tsx
+++ src/screens/profile/PetUpdate.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import {
-  View, Text, TextInput, TouchableOpacity, Image, StyleSheet, ScrollView, SafeAreaView, Alert
+  View, Text, TouchableOpacity, Image, ScrollView, SafeAreaView, Alert
 } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { launchImageLibrary } from 'react-native-image-picker';
@@ -8,6 +8,10 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import emptyCircleFrame from '@image/frame/addPetCircle.png';
 import { PetRegistRequest } from '@src/utils/constants/types';
 import { updatePetRegistration } from '@src/api/petApi';
+import CustomTextInput from '@src/components/common/CustomTextInput';
+import { RoundedCircleButton, RoundedTextButton } from '@src/components/common/RoundedButton';
+import StylizedText from '@src/components/common/StylizedText';
+import Avatar from '@src/components/common/Avatar';
 
 const PetUpdate = () => {
   const navigation = useNavigation();
@@ -72,8 +76,8 @@ const PetUpdate = () => {
     };
     try {
       await updatePetRegistration(petData, petId, navigation);
-  
-      // Update AsyncStorage with the latest pet data
+
+      // 최신 반려동물 데이터를 AsyncStorage에 업데이트
       const updatedPetData = {
         name,
         petType: petType === '강아지' ? 'DOG' : 'CAT',
@@ -84,7 +88,7 @@ const PetUpdate = () => {
         breed,
       };
       await AsyncStorage.setItem(`PET_${petId}`, JSON.stringify(updatedPetData));
-  
+
       Alert.alert('성공', '반려동물 정보가 업데이트되었습니다.');
       navigation.goBack();
     } catch (error) {
@@ -94,263 +98,100 @@ const PetUpdate = () => {
   };
 
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <ScrollView contentContainerStyle={styles.scrollViewContent}>
-        <View style={styles.container}>
-          <TouchableOpacity onPress={handleBackButton} style={styles.backButton}>
-            <Text style={styles.backButtonText}>{'<'}</Text>
+    <SafeAreaView className="flex-1 bg-white">
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }} className="p-5">
+        <View className="flex items-center">
+          {/* 뒤로 가기 버튼 */}
+          <TouchableOpacity onPress={handleBackButton} className="absolute top-5 left-5">
+            <Text className="text-2xl text-gray-500">{'<'}</Text>
           </TouchableOpacity>
-          
-          <TouchableOpacity onPress={handleImagePick} style={styles.imageContainer}>
-            <Image 
-              source={petImage ? { uri: petImage.uri } : emptyCircleFrame} 
-              style={styles.image} 
+
+          {/* 반려동물 이미지 */}
+          <View className="mb-6 mt-20">
+            <RoundedCircleButton color="bg-white" shadow={false} size={100} onPress={handleImagePick}>
+              <Avatar 
+                source={petImage ? { uri: petImage.uri } : emptyCircleFrame}  
+                size={150}
+              />
+            </RoundedCircleButton>
+            <View className="mt-10">
+              <StylizedText type="body1">눌러서 사진 변경</StylizedText>
+            </View>
+          </View>
+
+          {/* 입력 필드 */}
+          <CustomTextInput label="이름" placeholder="이름을 입력하세요" value={name} onChangeText={setName} keyboardType="default" />
+          <CustomTextInput label="견종/묘종" placeholder="견종 / 묘종을 입력하세요" value={breed} onChangeText={setBreed} keyboardType="default" />
+          <CustomTextInput label="체중(kg)" placeholder="체중 (kg)을 입력하세요" value={weight} onChangeText={setWeight} keyboardType="numeric" />
+          <CustomTextInput label="나이" placeholder="나이를 입력하세요" value={age} onChangeText={setAge} keyboardType="numeric" />
+
+          {/* 반려동물 타입 선택 */}
+          <View className="w-full flex-row justify-around items-center mb-5 pt-5">
+            <TouchableOpacity
+              onPress={() => setPetType('강아지')}
+              className="flex-row items-center"
+            >
+              <View className={`w-6 h-6 rounded-full border-2 border-skyblue justify-center items-center ${petType === '강아지' ? 'bg-blue' : ''}`}>
+                {petType === '강아지' && <View className="w-3 h-3 rounded-full bg-white" />}
+              </View>
+              <View className='pl-5'>
+                <StylizedText type='header2'>강아지</StylizedText>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => setPetType('고양이')}
+              className="flex-row items-center"
+            >
+              <View className={`w-6 h-6 rounded-full border-2 border-skyblue justify-center items-center ${petType === '고양이' ? 'bg-blue' : ''}`}>
+                {petType === '고양이' && <View className="w-3 h-3 rounded-full bg-white" />}
+              </View>
+              <View className='pl-5'>
+                <StylizedText type='header2'>고양이</StylizedText>
+              </View>
+            </TouchableOpacity>
+          </View>
+
+          {/* 성별 선택 */}
+          <View className="w-full flex-row justify-around items-center mb-5 ">
+            <TouchableOpacity
+              onPress={() => setGender('암')}
+              className="flex-row items-center -ml-6"
+            >
+              <View className={`w-6 h-6 rounded-full border-2 border-skyblue justify-center items-center ${gender === '암' ? 'bg-blue' : ''}`}>
+                {gender === '암' && <View className="w-3 h-3 rounded-full bg-white" />}
+              </View>
+              <View className='pl-5'>
+                <StylizedText type='header2'>암</StylizedText>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => setGender('수')}
+              className="flex-row items-center mr-2"
+            >
+              <View className={`w-6 h-6 rounded-full border-2 border-skyblue justify-center items-center ${gender === '수' ? 'bg-blue' : ''}`}>
+                {gender === '수' && <View className="w-3 h-3 rounded-full bg-white" />}
+              </View>
+              <View className='pl-5'>
+                <StylizedText type='header2'>수</StylizedText>
+              </View>
+            </TouchableOpacity>
+          </View>
+
+          {/* 저장하기 버튼 */}
+          <View className='pt-5'>
+            <RoundedTextButton
+              content={'저장하기'}
+              textType='header2'
+              widthOption="full"
+              color="bg-primary"
+              onPress={handleSubmit}
             />
-            <Text style={styles.imageText}>눌러서 사진 변경</Text>
-          </TouchableOpacity>
-          
-          <TextInput
-            style={styles.input}
-            placeholder="이름"
-            placeholderTextColor="#888"
-            value={name}
-            onChangeText={setName}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="견종 / 묘종"
-            placeholderTextColor="#888"
-            value={breed}
-            onChangeText={setBreed}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="체중 (kg)"
-            placeholderTextColor="#888"
-            keyboardType="numeric"
-            value={weight}
-            onChangeText={setWeight}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="나이"
-            placeholderTextColor="#888"
-            keyboardType="numeric"
-            value={age}
-            onChangeText={setAge}
-          />
-
-          <View style={styles.optionContainer}>
-            <View style={styles.optionButtons}>
-              <TouchableOpacity
-                style={styles.checkboxContainer}
-                onPress={() => setPetType('강아지')}
-              >
-                <View style={[styles.checkbox, petType === '강아지' && styles.checkedBox]}>
-                  {petType === '강아지' && <View style={styles.innerCircle} />}
-                </View>
-                <Text style={styles.checkboxLabel}>강아지</Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={styles.checkboxContainer}
-                onPress={() => setPetType('고양이')}
-              >
-                <View style={[styles.checkbox, petType === '고양이' && styles.checkedBox]}>
-                  {petType === '고양이' && <View style={styles.innerCircle} />}
-                </View>
-                <Text style={styles.checkboxLabel}>고양이</Text>
-              </TouchableOpacity>
-            </View>
           </View>
 
-          <View style={styles.spacer} />
-
-          <View style={styles.optionContainer}>
-            <View style={styles.optionButtons}>
-              <TouchableOpacity
-                style={styles.checkboxContainer}
-                onPress={() => setGender('암')}
-              >
-                <View style={[styles.checkbox, gender === '암' && styles.checkedBox]}>
-                  {gender === '암' && <View style={styles.innerCircle} />}
-                </View>
-                <Text style={styles.checkboxLabel}>암</Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={styles.checkboxContainerSex}
-                onPress={() => setGender('수')}
-              >
-                <View style={[styles.checkbox, gender === '수' && styles.checkedBox]}>
-                  {gender === '수' && <View style={styles.innerCircle} />}
-                </View>
-                <Text style={styles.checkboxLabel}>수</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-          
-          <TouchableOpacity style={styles.submitButton} onPress={handleSubmit}>
-            <Text style={styles.submitButtonText}>저장하기</Text>
-          </TouchableOpacity>
         </View>
       </ScrollView>
     </SafeAreaView>
   );
 };
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-  },
-  safeArea: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  scrollViewContent: {
-    flexGrow: 1,
-    justifyContent: 'center',
-  },
-
-  backButton: {
-    position: 'absolute',
-    top: 20,
-    left: 20,
-  },
-  backButtonText: {
-    fontSize: 24,
-    color: '#888',
-  },
-  imageContainer: {
-    alignItems: 'center',
-    marginTop: 50,
-    marginBottom: 30,
-  },
-  image: {
-    width: 150,
-    height: 150,
-    borderRadius: 100,
-  },
-  imageText: {
-    marginTop: 10,
-    color: '#888',
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#ddd',
-    borderRadius: 10,
-    paddingVertical: 10,
-    paddingHorizontal: 15,
-    marginBottom: 20,
-    width: '100%',
-    color: '#888',
-  },
-  optionContainer: {
-    marginBottom: 20,
-    width: '100%',
-  },
-  optionButtons: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  checkboxContainer: {
-    flexDirection: 'row',
-    paddingRight: 85,
-    alignItems: 'center',
-    marginHorizontal: 20,
-  },
-  checkboxContainerSex: {
-    flexDirection: 'row',
-    paddingLeft: 20,
-    alignItems: 'center',
-    marginHorizontal: 20,
-  },
-  checkbox: {
-    width: 24,
-    height: 24,
-    borderRadius: 12,
-    borderWidth: 2,
-    borderColor: 'skyblue',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginRight: 8,
-  },
-  checkedBox: {
-    backgroundColor: '#007AFF',
-  },
-  innerCircle: {
-    width: 12,
-    height: 12,
-    borderRadius: 6,
-    backgroundColor: '#fff',
-  },
-  checkboxLabel: {
-    fontSize: 16,
-    color: '#000',
-  },
-  submitButton: {
-    backgroundColor: 'skyblue',
-    borderRadius: 5,
-    paddingVertical: 15,
-    alignItems: 'center',
-    marginTop: 20,
-    width: '100%',
-  },
-  submitButtonText: {
-    color: '#fff',
-    fontWeight: 'bold',
-  },
-  spacer: {
-    height: 20,
-  },
-  modalContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  },
-  modalContent: {
-    backgroundColor: 'white',
-    borderRadius: 10,
-    padding: 20,
-    width: '80%',
-    maxHeight: '80%',
-  },
-  modalTitle: {
-    fontSize: 18,
-    color: 'black',
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  deviceItem: {
-    padding: 10,
-    borderBottomWidth: 1,
-    color: 'black',
-    borderBottomColor: '#ccc',
-  },
-  deviceId: {
-    fontSize: 12,
-    color: '#888',
-  },
-  emptyListText: {
-    textAlign: 'center',
-    marginTop: 20,
-  },
-  closeButton: {
-    marginTop: 20,
-    padding: 10,
-    backgroundColor: '#ddd',
-    borderRadius: 5,
-    alignItems: 'center',
-  },
-  closeButtonText: {
-    fontSize: 16,
-  },
-  bluetoothText: {
-    color: '#888',
-  },
-});
 
 export default PetUpdate;
