2a8017564f73d7f1b344248415dab94e191ddbb2
Wendy Nam <142412339+Wendy-Nam@users.noreply.github.com>
2024-11-12 04:36:00 +0900

    Design : 전반적인 컴포넌트 추가/수정
    <수정한 내용>
    
    1. ColorMap : tailwind 파일에서 커스텀색상 코드 직접 참조하도록.
    - tailwind 색상도 추가.
    - opacityMap 추가:
    커스텀색상의 hex코드에도 투명도 조절가능
    2. Modal : 배경과 모달분리
    3. ShadowBox : shadowbox를 wrapper로 쓰지않고, 그림자 스타일만을 직접 사용.
    - 혜인 스타일링 반영해 수정.
    4. RoundedBox : 전반적 최적화 - Frame 컴포넌트 없앰. Shadow 적용부분 수정. boxstyle 분리함 (+outline 옵션 추가)
    5. StylizedText
    - styleClass로 컬러포함 tailwind 스타일 받음.
    - 커스텀텍스트 type 텍스트포맷 추가&수정
    
    <추가한 내용>
    - Avatar, Badge, RoundedButton, RadioButton(+Group), Loading(ProgressDots), ModalLayout(모달 내부배치 wrapper), InfoCards

--- src/components/common/ShadowBox.tsx
+++ src/components/common/ShadowBox.tsx
@@ -1,34 +1,24 @@
+// ShadowBox.tsx
 import React from 'react';
-import { Shadow } from 'react-native-shadow-2';
+import { View } from 'react-native';
 
 type ShadowBoxProps = {
-  children: React.ReactNode; // Accepts any children components
-  distance?: number; // Distance for the shadow
-  startColor?: string; // Starting color for the shadow
-  endColor?: string; // Ending color for the shadow
-  offset?: [number, number]; // Offset for the shadow
-  style?: any; // Additional styles for the shadow
+  children: React.ReactNode;
+  style?: any;
 };
 
-const ShadowBox: React.FC<ShadowBoxProps> = ({
-  children,
-  distance = 4.5,
-  startColor = 'rgba(0, 0, 0, 0.06)',
-  endColor = 'rgba(0, 0, 0, 0)',
-  offset = [0, 3],
-  style,
-}) => {
+export const ShadowStyle = {
+  shadowColor: '#000',
+  shadowOffset: { width: 0, height: 4 },
+  shadowOpacity: 0.15,
+  shadowRadius: 8,
+  elevation: 4.5,
+};
+
+export const ShadowBox: React.FC<ShadowBoxProps> = ({ children, style }) => {
   return (
-    <Shadow
-      distance={distance}
-      startColor={startColor}
-      endColor={endColor}
-      offset={offset}
-      style={style}
-    >
+    <View style={[style, ShadowStyle]}>
       {children}
-    </Shadow>
+    </View>
   );
 };
-
-export default ShadowBox;
--- src/components/common/StylizedText.tsx
+++ src/components/common/StylizedText.tsx
@@ -1,25 +1,35 @@
 import React from 'react';
 import { Text } from 'react-native';
-import { TextProps, HeaderTextProps } from '@types';
+// import { TextProps, HeaderTextProps } from '@types';
 
-const StylizedText: React.FC<TextProps> = ({ type, children, color = 'text-black' }) => {
+type TextProps = {
+  type?: string;
+  children?: React.ReactNode;
+  styleClass?: string;  
+};
+
+interface HeaderTextProps {
+  text: string;
+  highlight?: string;
+}
+
+const StylizedText: React.FC<TextProps> = ({ type = 'body1', children, styleClass }) => {
   const styles = getStyles(type);
   return (
-    <Text className={color} style={styles}>{children}</Text>
+    <Text className={styleClass} style={styles}>{children}</Text>
   );
 };
 
 export const HeaderText: React.FC<HeaderTextProps> = ({
   text,
-  highlight,
+  highlight = '',
 }) => {
   // 강조할 부분을 찾아서 분리
   const parts = text.split(highlight);
-
   return (
     <Text className="my-2 mb-4 ml-2 text-xl" style={getStyles('header1')}>
       {parts.map((part, index) => (
-        <Text key={index} className="text-black">
+        <Text key={index} className="text-black" style={getStyles('header1')}>
           {part}
           {index < parts.length - 1 && (
             <Text className="text-primary">{highlight}</Text> // primary 컬러
@@ -30,45 +40,113 @@ export const HeaderText: React.FC<HeaderTextProps> = ({
   );
 };
 
+interface StyledTextStyle {
+  fontFamily: string;
+  fontSize: number;
+}
+
 // Function to return the styles based on the type
-const getStyles = (type: string) => {
+export const getStyles = (type: string) : StyledTextStyle => {
   switch (type) {
     case 'header1':
       return {
         fontFamily: 'Pretendard-Bold',
-        fontSize: 20,
-        lineHeight: 30,
+        fontSize: 22,
       };
     case 'header2':
       return {
-        fontFamily: 'Pretendard-SemiBold',
+        fontFamily: 'Pretendard-Bold',
         fontSize: 16,
-        lineHeight: 22,
+      };
+    case 'header3':
+      return {
+        fontFamily: 'Pretendard-Medium',
+        fontSize: 12,
+      };
+    case 'header4': // 질병정보카드의 제목
+      return {
+        fontFamily: 'Pretendard-Black',
+        fontSize: 18,
+      };
+    case 'header5': // 질병정보카드의 두꺼운 퍼센트숫자
+      return {
+        fontFamily: 'Pretendard-SemiBold',
+        fontSize: 18,
+      };
+      case 'header6': 
+      return {
+        fontFamily: 'Pretendard-Bold',
+        fontSize: 18,
+      };
+      case 'header7':
+      return {
+        fontFamily: 'Pretendard-ExtraBold',
+        fontSize: 18,
       };
     case 'body1':
       return {
-        fontFamily: 'Pretendard-Regular',
-        textColor: '#000',
+        fontFamily: 'Pretendard-Medium',
         fontSize: 14,
-        lineHeight: 20,
       };
     case 'body2':
       return {
-        fontFamily: 'Pretendard-ExtraLight',
+        fontFamily: 'Pretendard-Medium',
         fontSize: 12,
-        lineHeight: 18,
       };
-    case 'caption':
+    case 'body3':
       return {
-        fontFamily: 'Pretendard-Thin',
+        fontFamily: 'Pretendard-Medium',
         fontSize: 10,
-        lineHeight: 14,
+    };
+    case 'caption-title':
+      return {
+        fontFamily: 'Pretendard-Medium',
+        fontSize: 15,
+    };
+    case 'caption-label':
+      return {
+        fontFamily: 'Pretendard-Regular',
+        fontSize: 10.5,
+    };
+    case 'label':
+        return {
+        fontFamily: 'Pretendard-Regular',
+        fontSize: 9,
       };
+    case 'label1':
+      return {
+      fontFamily: 'Pretendard-Bold',
+      fontSize: 9,
+    };
+    case 'label2':
+      return {
+      fontFamily: 'Pretendard-Medium',
+      fontSize: 11,
+    };
+    case 'label3': // 질병정보카드 - 퍼센트
+      return {
+      fontFamily: 'Pretendard-Bold',
+      fontSize: 11,
+    };
+    case 'label4': // 질병정보카드 - 퍼센트라는 텍스트라벨
+      return {
+      fontFamily: 'Pretendard-Bold',
+      fontSize: 6,
+    };
+    case 'record1':
+      return {
+      fontFamily: 'Pretendard-Bold',
+      fontSize: 13,
+    };
+    case 'record2':
+      return {
+      fontFamily: 'Pretendard-Medium',
+      fontSize: 13,
+    };
     default:
       return {
         fontFamily: 'Pretendard-Regular',
         fontSize: 14,
-        lineHeight: 20,
       };
   }
 };
