c3e2de5bb2dcbd91f61d2af92d78acfd9328f352
Wendy Nam <142412339+Wendy-Nam@users.noreply.github.com>
2024-11-11 23:11:32 +0900

    Feat : 개발환경 설정 추가 (경로 alias, nativewind 관련)
    UI관련:
    tailwindcss(nativewind) 설정과 커스텀색상명 추가.
    
    경로관련:
    - src 디렉토리 내부에 소스코드 정리
    - api, components, screens, store, utils의 5가지 분류로 하위디렉토리 분류.

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,26 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['module:@react-native/babel-preset'],
-    plugins: ['react-native-reanimated/plugin', "nativewind/babel"],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],  // src 디렉토리를 루트로 설정
+          alias: {
+            '@api': './src/api',
+            '@data': './src/api/data',
+            '@components': './src/components',
+            '@common': './src/components/common',
+            '@screens': './src/screens',
+            '@constants': './src/utils/constants',
+            '@types': './src/utils/contsants/types',
+            '@image': './src/assets/image',
+            // 필요한 alias를 더 추가할 수 있습니다.
+          },
+        },
+      ],
+      'react-native-reanimated/plugin',
+      'nativewind/babel',
+    ],
   };
 };
