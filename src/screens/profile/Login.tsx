a7db094c4f01d65e0ec14e66a15aa523a8ad7a30
Wendy Nam <142412339+Wendy-Nam@users.noreply.github.com>
2024-12-01 23:56:25 +0900

    Design : 로그인 페이지 UI 수정
    작업자 : @Hyeeinn
    
    Co-Authored-By: Hyeeinn <148345481+Hyeeinn@users.noreply.github.com>

--- src/screens/profile/Login.tsx
+++ src/screens/profile/Login.tsx
@@ -3,6 +3,8 @@ import { View, Image, StyleSheet, TouchableOpacity, Modal, ActivityIndicator } f
 import { useNavigation, NavigationProp } from '@react-navigation/native';
 import KakaoLogo from '@image/icon/logo-kakao.png';
 import NaverLogo from '@image/icon/logo-naver.png';
+import LoginCat from '@image/login_cat.png';
+import LoginDog from '@image/login_dog.png';
 import { WebView, WebViewNavigation } from 'react-native-webview';
 import { handleLoginPress, handleWebViewNavigationStateChange } from '@api/loginApi';
 import StylizedText from '@components/common/StylizedText';
@@ -15,32 +17,45 @@ const Login = () => {
 
   return (
     <View style={styles.container}>
+      {/* 상단 영역 */}
       <View style={styles.topHalf}>
-        <View style={styles.subtitleContainer}>
-        <StylizedText type="header1" styleClass="text-white">
-          {'현명하게\n우리 아이\n건강 관리'}
-        </StylizedText>
-          <StylizedText type="body1" styleClass="text-white">
-            {'우리 아이의 \n마음상태 몸상태를 알 수 있는 \n가장 정확하고 간편한 방법'}
+        <View style={styles.headerContainer}>
+          <StylizedText type="header0" styleClass="text-white mb-4">
+            {'현명하게\n우리 아이 건강 관리'}
           </StylizedText>
+          <StylizedText type="default" styleClass="text-white">
+            {'우리 아이의\n마음 상태 몸 상태를 알 수 있는\n가장 정확하고 간편한 방법 '}
+          </StylizedText>
+        </View>
+        {/* 이미지 중앙 배치 */}
+        <View style={styles.imageRow}>
+          <Image source={LoginDog} style={styles.animalImage} />
+          <Image source={LoginCat} style={styles.animalImage} />
         </View>
       </View>
+      {/* 하단 영역 */}
       <View style={styles.bottomHalf}>
-        <View style={styles.buttonTextContainer}>
-          <StylizedText type="body1" styleClass="text-black">
-            10초만에 회원가입
-          </StylizedText>
-        </View>
         <View style={styles.buttonContainer}>
-          <TouchableOpacity onPress={() => handleLoginPress('kakao', setLoginUrl, setShowWebView)}>
+          <TouchableOpacity
+            onPress={() => handleLoginPress('kakao', setLoginUrl, setShowWebView)}
+            style={styles.loginButtonKakao}>
             <Image source={KakaoLogo} style={styles.buttonImage} />
+            <StylizedText type="body2" styleClass="text-black ml-2">
+              카카오로 로그인하기
+            </StylizedText>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => handleLoginPress('naver', setLoginUrl, setShowWebView)}>
+          <TouchableOpacity
+            onPress={() => handleLoginPress('naver', setLoginUrl, setShowWebView)}
+            style={styles.loginButtonNaver}>
             <Image source={NaverLogo} style={styles.buttonImage} />
+            <StylizedText type="body2" styleClass="text-white ml-2">
+              네이버로 로그인하기
+            </StylizedText>
           </TouchableOpacity>
         </View>
       </View>
 
+      {/* 웹뷰 모달 */}
       <Modal visible={showWebView} animationType="slide">
         <View style={{ flex: 1 }}>
           {loading && <ActivityIndicator color="blue" size="large" />}
@@ -64,50 +79,59 @@ const Login = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'skyblue',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#73A8BA',
   },
   topHalf: {
     flex: 1,
-    width: '100%',
+    justifyContent: 'flex-end',
+    paddingBottom: 160, // 텍스트와 이미지를 조정하는 공간
+  },
+  headerContainer: {
+    paddingHorizontal: 30,
+    paddingBottom: 90,
+  },
+  imageRow: {
+    flexDirection: 'row',
     justifyContent: 'center',
-    alignItems: 'center',
+    marginBottom: -160,
+  },
+  animalImage: {
+    width: 140,
+    height: 140,
+    marginHorizontal: 50,
   },
   bottomHalf: {
     flex: 1,
-    width: '100%',
     backgroundColor: 'white',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-  subtitleContainer: {
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    width: '80%',
-    marginBottom: 60,
-  },
-  buttonTextContainer: {
-    padding: 10,
-    borderRadius: 20,
-    marginBottom: 20,
-    backgroundColor: 'rgba(0, 0, 0, 0)',
-    borderWidth: 1,
-    borderColor: 'grey',
-    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    paddingVertical: 20,
   },
   buttonContainer: {
+    width: '80%',
+  },
+  loginButtonKakao: {
     flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FEE500',
+    paddingVertical: 15,
+    borderRadius: 30,
+    marginBottom: 10,
+  },
+  loginButtonNaver: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#03C75A',
+    paddingVertical: 15,
+    borderRadius: 30,
   },
   buttonImage: {
-    width: 50,
-    height: 50,
-    margin: 5,
+    width: 24,
+    height: 24,
+    marginRight: 8,
   },
-  closeButton: {
-    padding: 10,
-    alignItems: 'center',
-  }
 });
 
 export default Login;
